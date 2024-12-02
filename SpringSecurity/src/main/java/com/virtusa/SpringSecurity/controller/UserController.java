9af4367755673ba8a52a358c0dda83debf09cc0b
vaishu1427 <vaishnavi.senthil1427@gmail.com>
2024-12-02 20:13:03 +0530

    Added mappings to the user

--- SpringSecurity/src/main/java/com/virtusa/SpringSecurity/controller/UserController.java
+++ SpringSecurity/src/main/java/com/virtusa/SpringSecurity/controller/UserController.java
@@ -1,12 +1,23 @@
 package com.virtusa.SpringSecurity.controller;
 
+import com.virtusa.SpringSecurity.model.User;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class UserController {
+
+    private List<User> users = new ArrayList<>(List.of(
+            new User(1,"Vaishu","Developer"),
+            new User(2,"Ammu","Tester")
+    ));
 
     @GetMapping("/")
     public String getMessage(HttpServletRequest request) {
@@ -16,5 +27,16 @@ public class UserController {
     @GetMapping("/user")
     public String getMessage1(HttpSession session) {
         return "Hi "+ session.getId();
+    }
+
+    @GetMapping("/getListOfusers")
+    public List<User> getListOfUsers(){
+        return users;
+    }
+
+    @PostMapping("/addUser")
+    public List<User> addUser(@RequestBody User user){
+        users.add(user);
+        return users;
     }
 }
--- SpringSecurity/src/main/java/com/virtusa/SpringSecurity/model/User.java
+++ SpringSecurity/src/main/java/com/virtusa/SpringSecurity/model/User.java
@@ -1,4 +1,21 @@
 package com.virtusa.SpringSecurity.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class User {
+
+    private int id;
+    private String name;
+    private String designation;
+
+    public User(int id, String name, String designation) {
+        this.name = name;
+        this.designation = designation;
+        this.id = id;
+    }
+
+
 }
