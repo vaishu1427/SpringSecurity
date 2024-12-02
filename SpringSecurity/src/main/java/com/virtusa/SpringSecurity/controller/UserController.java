45a0e3ea8636c66e495955a285190f4630ddc96b
vaishu1427 <vaishnavi.senthil1427@gmail.com>
2024-12-02 18:57:59 +0530

    Configured with our username and password

--- SpringSecurity/src/main/java/com/virtusa/SpringSecurity/controller/UserController.java
+++ SpringSecurity/src/main/java/com/virtusa/SpringSecurity/controller/UserController.java
@@ -1,5 +1,7 @@
 package com.virtusa.SpringSecurity.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,7 +9,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
 
     @GetMapping("/")
-    public String getMessage(){
-        return "Anyway it's gonna be hard. Let's face it.";
+    public String getMessage(HttpServletRequest request) {
+        return "Anyway it's gonna be hard. Let's face it. "+ request.getSession().getId();
+    }
+
+    @GetMapping("/user")
+    public String getMessage1(HttpSession session) {
+        return "Hi "+ session.getId();
     }
 }
